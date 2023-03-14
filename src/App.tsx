36147285294ad3dba56ebe411093cf9e9060b4eb
adiaz111 <adiaz8@madisoncollege.edu>
2023-03-14 10:58:26 -0500

    Added dummy Home component...
    With some bootstrap styled items.
    Button and Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CardHeader from "react-bootstrap/esm/CardHeader";
+// import CardHeader from "react-bootstrap/esm/CardHeader";
+
+const Home = () => (
+  <>
+    <h1>TCA Bar React TS Bootstrap</h1>
+    <h2>Companion App</h2>
+    <Button variant="outline-primary">Play Bar</Button>
+    <Card>
+      <CardHeader>LeaderBoard</CardHeader>
+      <Card.Body>Play a gmae ot see you leaderboard...</Card.Body>
+    </Card>
+  </>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
