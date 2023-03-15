a4f91f87b59a490719a3164d4d2b9d43b6440336
adiaz111 <adiaz8@madisoncollege.edu>
2023-03-15 12:15:22 -0500

    week 07 thu meeting

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,43 @@ import { Play } from "./Play";
 
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import { GameResult, calculateLeaderboard } from "./front-end-model";
+import { useState } from "react";
+
+const hardcodedGameResults: GameResult[] = [
+  {
+    winner: "Tom",
+    players: ["Tom", "Taylor"],
+  },
+  {
+    winner: "Taylor",
+    players: ["Jack", "Taylor"],
+  },
+  {
+    winner: "Taylor",
+    players: ["Tom", "Taylor", "Jack"],
+  },
+  {
+    winner: "X",
+    players: ["X", "Joe"],
+  },
+  {
+    winner: "X",
+    players: ["X", "Joe"],
+  },
+  {
+    winner: "Joe",
+    players: ["X", "Joe"],
+  },
+  {
+    winner: "Jack",
+    players: ["X", "Joe", "Jack"],
+  },
+];
+
+const App = () => {
+  const [results, setGameResults] = useState(hardcodedGameResults);
+
   return (
     <div className="App m-3">
       <h1>TCA Bar React TS Bootstrap</h1>
@@ -15,13 +51,16 @@ function App() {
       <hr />
       <HashRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={<Home leaderboardData={calculateLeaderboard(results)} />}
+          />
           <Route path="/setup" element={<Setup />} />
           <Route path="/play" element={<Play />} />
         </Routes>
       </HashRouter>
     </div>
   );
-}
+};
 
 export default App;
